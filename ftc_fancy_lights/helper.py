21b271a9e258c7734fd0061179dff062e44724ce
aask <ameliawietting@Teapot3.lan>
2024-02-26 18:38:25 -0600

    Removed things

---
+++ ftc_fancy_lights/helper.py
@@ -0,0 +1,13 @@
+def hsv_to_rgb(h, s, v):
+    if s == 0.0: return (v, v, v)
+    i = int(h*6.)
+    f = (h*6.)-i
+    p,q,t = int(255*(v*(1.-s))), int(255*(v*(1.-s*f))), int(255*(v*(1.-s*(1.-f))))
+    v = int(255*v)
+    i %= 6
+    if i == 0: return (v, t, p)
+    if i == 1: return (q, v, p)
+    if i == 2: return (p, v, t)
+    if i == 3: return (p, q, v)
+    if i == 4: return (t, p, v)
+    if i == 5: return (v, p, q)
--- ftc_fancy_lights/led_manager.py
+++
@@ -1,5 +0,0 @@
-from plasma import plasma_stick
-
-NUM_LEDS = 144
-LED_PIN = 28
-SPEED_MOD_MS_PER_LED = 0
