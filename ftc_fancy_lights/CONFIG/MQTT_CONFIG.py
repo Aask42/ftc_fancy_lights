c0ef7deaab6d96bd880c4d2aa9f192b09107e1bd
aask <ameliawietting@Teapot3.iowa.stir.org>
2024-02-28 18:30:23 -0600

    adding more

---
+++ ftc_fancy_lights/CONFIG/MQTT_CONFIG.py
@@ -0,0 +1,8 @@
+## This area of the configuration will come pre-set on your device. 
+## DO NOT CHANGE THIS UNLESS INSTRUCTED TO DO SO BY AMELIA WIETTING OR TODD VOLZ
+
+MQTT_USERNAME=b"test_1" # These need to be in BYTES
+MQTT_PASSWORD=b"test1234!" # These need to be in BYTES
+MQTT_SERVER=b"URL"
+MQTT_CLIENT_ID="test_1"
+# This was from an OTA update ;)
